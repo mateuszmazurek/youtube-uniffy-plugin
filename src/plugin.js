3d56f83f939462f3419ac451cf6e272f77c457f6
Mateusz Mazurek <mateusz.mazurek@apilabs.pl>
2014-07-13 14:32:41 +0200

    First commit

---
+++ src/plugin.js
@@ -0,0 +1,42 @@
+function search(query, searchID){
+	
+	// DO SOMETHING HERE
+
+	/*
+
+	...
+	$U.registerResult(new MusicResult(...), searchID);
+	...
+
+	$U.finished(searchID);
+
+	*/
+
+}
+
+function getPlayURL(result, returnURL, fail, task){
+
+	/*
+
+	You can show user info what your plugin is doing at the moment:
+
+	$U.info('Doing something...', {
+		context: $U.Context.getPlayURL,
+		task: task
+	});
+
+	*/
+
+	// DO SOMETHING HERE
+
+	/*
+
+	Return direct link to file:
+	returnURL('http://server.com/file.mp3')
+
+	Or via Uniffy local proxy:
+	returnURL($U.proxy(anyURL));
+
+	*/
+
+}
